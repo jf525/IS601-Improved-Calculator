b3c3a58bb10a642742bf40a50755814bbaff8478
jf525 <jf525@njit.edu>
2024-06-13 21:21:04 -0400

    Added caclulation and operations python files. Added multiplication and division in the calculator and tests. Ran tests and all tests passed successfully

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
     '''Testing that the addition function works'''
@@ -8,4 +8,12 @@ def test_addition():
 def test_subtraction():
     '''Testing that the subtaction function works'''
     assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Testing that the multiplication function works'''
+    assert multiply(2,3) == 6
+
+def test_division():
+    '''Testing that the division function works'''
+    assert divide(8,2) == 4
     
