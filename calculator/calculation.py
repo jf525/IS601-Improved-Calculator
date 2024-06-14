0877816ca79595d102d388d27b1189cd2fe21810
jf525 <jf525@njit.edu>
2024-06-13 21:43:01 -0400

    Added divide by zero error. Added factory in calculation.py

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -3,6 +3,10 @@ class Calculation:
         self.a = a
         self.b = b
         self.operation = operation
+    
+    @staticmethod
+    def create(a, b, operation):
+        return  Calculation(a, b, operation)
 
     def get_calculation_result(self):
         return self.operation(self.a, self.b)
